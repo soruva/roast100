58b2f78a67330beb4c5bb256480923e00dfd3c00
SoruvaLab <soruvalab@gmail.com>
2026-04-20 23:49:29 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, boxSizing: "border-box" }}>
+        {children}
+      </body>
     </html>
   );
 }
